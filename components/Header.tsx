fd9c69d248d81488c4267f5c1f24d631769c211a
Cynto <lucagaravello@outlook.com>
2022-01-24 02:00:36 +0100

    Replaced menu icon with svg taken straight from youtube, added search icon next to searchbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import style from '../styles/Header.module.css';
 import Image from 'next/image';
 import youtubeLogo from '../public/images/YouTube-Logo-edited.wine.png';
+import searchIcon from '../public/images/search-icon.svg';
+import menuIcon from '../public/images/menu-icon.svg';
 
 function Header() {
   return (
     <header className={style.header}>
-      <i className="fas fa-bars" />
+      <Image src={menuIcon} height={24} width={24} alt="menu icon" />
 
       <div className={style['youtube-logo-container']}>
         <Image
@@ -20,6 +22,9 @@ function Header() {
       <div className={style['input-container']}>
         <form action="/results">
           <input type="text" placeholder="Search" />
+          <button title="Search">
+            <Image src={searchIcon} alt="search icon" />
+          </button>
         </form>
       </div>
     </header>
